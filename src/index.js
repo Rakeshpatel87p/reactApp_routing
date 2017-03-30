b9ba71ea544a393f52a5de3c80bf82f4d5fd5005
Rakesh Patel <emailme@rakeshrpatel.com>
2017-03-30 09:51:12 -0400

    inital commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ModalGallery from './config/routes';
+import './index.css';
+
+ReactDOM.render(
+  <ModalGallery />,
+  document.getElementById('root')
+);
